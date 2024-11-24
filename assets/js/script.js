49674b1fb38a6f3da5d640e15f0e7c852c940621
Noman Rafiq <heyimnomee@gmail.com>
2024-11-24 11:28:32 +0500

    nav and togglers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,30 @@
-const navCloseBtn = document.querySelector('[data-nav-toggler]');
+const navTogglers = document.querySelectorAll('[data-nav-toggler]');
 const navbar = document.querySelector('[data-navbar]');
+const overlay = document.querySelector('[data-overlay]');
 
-navCloseBtn.addEventListener('click', (e) => {
-    e.target.classList.remove('active');
-    navbar.classList.remove('active');
+const addEventsOnElements = function (elements, eventType, callback) {
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener(eventType, callback);
+        console.log(elements[i]);
+    }
+};
+console.log(navTogglers);
+
+const toggleNavbar = function () {
+    navbar.classList.toggle('active');
+    overlay.classList.toggle('active');
+    document.body.classList.toggle('nav-active');
+};
+
+addEventsOnElements(navTogglers, 'click', toggleNavbar);
+
+// Header
+
+const header = document.querySelector('[data-header]');
+window.addEventListener('scroll', function () {
+    if (this.window.scrollY > 100) {
+        header.classList.add('active');
+    } else {
+        header.classList.remove('active')
+    }
 });
