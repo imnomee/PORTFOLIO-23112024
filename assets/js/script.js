dd415ca672e58710281839e4d964726075d0326c
Noman Rafiq <heyimnomee@gmail.com>
2024-11-25 13:34:15 +0500

    working

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,8 @@ const overlay = document.querySelector('[data-overlay]');
 const addEventsOnElements = function (elements, eventType, callback) {
     for (let i = 0; i < elements.length; i++) {
         elements[i].addEventListener(eventType, callback);
-        console.log(elements[i]);
     }
 };
-console.log(navTogglers);
 
 const toggleNavbar = function () {
     navbar.classList.toggle('active');
@@ -25,6 +23,30 @@ window.addEventListener('scroll', function () {
     if (this.window.scrollY > 100) {
         header.classList.add('active');
     } else {
-        header.classList.remove('active')
+        header.classList.remove('active');
     }
 });
+
+const revealElements = document.querySelectorAll('[data-reveal]');
+
+const revealDelayElements = document.querySelectorAll('[data-reveal-delay]');
+
+const reveal = function () {
+    for (let i = 0; i < revealElements.length; i++) {
+        if (
+            revealElements[i].getBoundingClientRect().top <
+            window.innerHeight / 1.2
+        ) {
+            revealElements[i].classList.add('revealed');
+        } else {
+            revealElements[i].classList.remove('revealed');
+        }
+    }
+};
+
+for (let i = 0; i < revealDelayElements.length; i++) {
+    revealDelayElements[i].style.transitionDelay =
+        revealDelayElements[i].dataset.revealDelay;
+}
+window.addEventListener('scroll', reveal);
+window.addEventListener('load', reveal);
